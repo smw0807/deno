bc6a01cc6e9b5832c6fe66c1ed1bbf33d02119dc
smw0807 <smw0807@gmail.com>
2024-10-21 15:46:55 +0900

    deno 노드 패키지

--- deno-project/main.ts
+++ deno-project/main.ts
@@ -1,10 +1,17 @@
 import dayjs from 'npm:dayjs';
+import * as fs from 'node:fs';
 export function add(a: number, b: number): number {
   return a + b;
 }
+
+const readFile = () => {
+  const data = fs.readFileSync('./deno.json', 'utf8');
+  console.log(data);
+};
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   console.log('Add 2 + 3 =', add(2, 3));
   console.log('Today is', dayjs().format('YYYY-MM-DD'));
+  // readFile();
 }
